9db3d5aaff9f47ba0c18f7987dc5e2fc49495ee2
Blake Alderink <blake.alderink@gmail.com>
2023-03-09 20:32:21 -0800

    more testing library completed

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -41,11 +41,45 @@ test("renders THREE error messages if user enters no values into any fields.", a
   expect(errorMessages).toHaveLength(3);
 });
 
-test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {});
+test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {
+  render(<ContactForm />);
+  const firstNameInput = screen.getByLabelText(/first name*/i);
+  const lastNameInput = screen.getByLabelText(/last name*/i);
+  userEvent.type(firstNameInput, "blake");
+  userEvent.type(lastNameInput, "alderink");
 
-test('renders "email must be a valid email address" if an invalid email is entered', async () => {});
+  const submit = screen.getByText(/submit/i);
+  userEvent.click(submit);
+  const error = await screen.findByText(/error/i);
 
-test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {});
+  expect(error).toBeInTheDocument();
+});
+
+test('renders "email must be a valid email address" if an invalid email is entered', async () => {
+  render(<ContactForm />);
+  const emailInput = screen.getByLabelText(/email*/i);
+
+  userEvent.type(emailInput, "hibutt");
+
+  const emailError = await screen.findByText(
+    /email must be a valid email address/i
+  );
+
+  expect(emailError).toBeInTheDocument();
+});
+
+test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+  render(<ContactForm />);
+
+  const submit = screen.getByText(/submit/i);
+  userEvent.click(submit);
+
+  const lastNameError = await screen.findByText(
+    /lastName is a required field/i
+  );
+
+  expect(lastNameError).toBeInTheDocument();
+});
 
 test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {});
 
